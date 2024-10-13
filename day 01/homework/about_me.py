4260df6c2c72ed47d22aab63311df66a7eee577b
Maskell619 <lukagetsadze9@gmail.com>
2024-10-13 13:52:53 +0400

    commit

---
+++ day 01/homework/about_me.py
@@ -0,0 +1,10 @@
+name = "Luka"
+age = 12
+
+last_name = "Getsadze"
+
+height = 152.5
+
+knows_programming = True
+
+print("My name is", name + ',', "last name:" + ' ' + last_name +  ",", age, "years old, my height is", height, 'and i know programming.')
