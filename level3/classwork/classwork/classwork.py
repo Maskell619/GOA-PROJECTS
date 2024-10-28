e27f94cf56f0f1b7e4476ce9edc6493b1a57de32
LukaGetsadze <lukagetsadze9@gmail.com>
2024-10-28 21:57:35 +0400

    commit

---
+++ level3/classwork/classwork/classwork.py
@@ -0,0 +1,17 @@
+name = "Luka"
+lastname = "Getsadze"
+
+age = 12
+
+fav_animal = "dog"
+
+print(name)
+print(lastname)
+
+print(age)
+
+print(fav_animal)
+
+print(1+1)
+print(2+5)
+print(14-9)
---
+++ level3/homework/homework.py
@@ -0,0 +1,39 @@
+name = "Luka"
+lastname = "Getsadze"
+
+age = 12
+
+hobby = "Programming"
+
+print(name)
+print(lastname)
+
+print(age)
+
+print(hobby)
+
+number1 = 2
+number2 = 3
+
+print(number1 + number2)
+
+number3 = 7
+number4 = 4
+
+print(number3-number4)
+
+print(14+4)
+print(7+3)
+
+print(5-2)
+print(6-1)
+
+print(6*8)
+print(9*8)
+
+print(7//7)
+print(6//3)
+
+print(4**2)
+print(7**6)
+
