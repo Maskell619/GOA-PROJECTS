593dbb66c2520575c17b2f444e329d715da048a2
Maskell619 <lukagetsadze9@gmail.com>
2024-11-13 21:04:45 +0400

    commit

---
+++ level5/classwork/cw.py
@@ -0,0 +1,49 @@
+# 1
+greeting = "Hello"
+userName = "student"
+age = '20'
+location = "New York"
+
+#2
+username = "Luka"
+print('Welcome, ' + username + "!" ) 
+print(greeting + ", " + username + "!") 
+
+#3
+print("Your age is " + age)  
+print("In 5 years, you will be " + str(int(age) + 5) +  " years old") 
+
+#4
+favoriteNumber = 7
+print("Your favorite number doubled is " + str(favoriteNumber * 2))
+
+#5
+total_score = 100
+bonus_points = 10
+finalScore = total_score + bonus_points
+print("Your final score is " + str(finalScore))
+
+#6
+days_in_a_week = 7
+print("There are " + str(days_in_a_week * 4) + " days in four weeks")
+
+#7
+firstName = "John"
+lastName = "Cena"
+print("Hello, " + firstName + " " + lastName + "!")
+
+#8
+nickname = "Ace"
+print("Your nickname is: " + nickname)
+
+#9
+
+#10
+year = 2024
+int(age)
+print("Next year is " + str(year + 1))
+print("Your age next year will be " + str(int(age) + 1))
+
+#11
+farewell = "Goodbye"
+print(farewell, username + "! See you next time.")
