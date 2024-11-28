bf2c24ebdb1d0116b9bd9c9c30a8c2cebb236adf
Maskell619 <lukagetsadze9@gmail.com>
2024-11-28 17:32:51 +0400

    first commit

---
+++ level7/homework/hw.py
@@ -0,0 +1,20 @@
+# user's name, lastname and age
+
+name = input("Enter your name: ")
+lastname = input("Enter your Lastname: ")
+age = input("Enter your age:")
+
+# input of 2 integers from  the user
+number1 = int(input("Enter any number: "))
+number2 = int(input("Enter any number: "))
+
+number1 * number2
+number1 // number2
+
+number1 + number2
+number2 + number1
+
+# email and password
+
+email = input("Enter you email: ")
+password = input("Enter your password: ")
