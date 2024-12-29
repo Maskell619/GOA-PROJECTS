456d4d77ddff45ca7d2b19936af9a84331ce4ff9
Maskell619 <lukagetsadze9@gmail.com>
2024-12-29 14:44:34 +0400

    commit

---
+++ level9/classwork/cw.py
@@ -0,0 +1,13 @@
+input1 = float(input("Enter any number: "))
+input2 = float(input("Enter any number: "))
+
+print(input1 * input2)
+print(input1/input2)
+
+print(input1 + input2)
+print(input1 - input)
+
+# კონკატენაცია ნიშნავს სტრიქონების გაერთიანებას
+
+# int ფუნქცია ნიშნავს მთელ რიცხვს, ან რომელიმე მონაცემის მთელ რიცხვად გადაყვანა
+# float ფუნქცია ნიშნავს რაციონალურ რიცხვს, მაგალითად: 1.2, 5.6, 7.0 ან შეგვიძლია ინტეჯერი გადავიყვანოთ ათწილადში
